5b493bd7f5e649b4b7167299a7c2774ba678df91
Robin Luo <RPHLuo@github.com>
2017-10-04 16:21:27 -0400

    added connector for search provider for basic search

--- ionic-BigBlueTutor/src/pages/home/home.ts
+++ ionic-BigBlueTutor/src/pages/home/home.ts
@@ -16,7 +16,18 @@ export class HomePage {
   categories;
   tutors;
   tutorsData;
+  searchCategories;
+  searchTutors;
+  imageLocations;
   constructor(public navCtrl: NavController, public events: Events, private ds: DsService, private rls:RecordListenService) {
+    this.imageLocations = [
+      "./assets/icon/math.png",
+      "./assets/icon/language.png",
+      "./assets/icon/social.png",
+      "./assets/icon/science.png",
+      "./assets/icon/art.png",
+      "./assets/icon/business.png"
+    ]
     var categoryData = ds.dataRecord.get('categories');
     this.categories = [];
     this.tutorsData = {};
@@ -36,6 +47,30 @@ export class HomePage {
   }
 
   onInput(event) {
+    this.ds.dsInstance.rpc.make('search', {param: this.search}, function(error, data) {
+      this.searchTutors = data.data;
+      console.log(this.searchTutors)
+    })
+    var categoryData = this.ds.dataRecord.get('categories');
+    this.searchCategories = [];
+    for (var category in categoryData) {
+      this.searchCategories.push(category);
+    }
+    for (var category in categoryData) {
+      var subCategories = categoryData[category];
+      for (var i =0;i<subCategories.length;i++) {
+        this.searchCategories.push(subCategories[i]);
+      }
+    }
+    this.searchCategories = this.searchCategories.filter(function(text) {
+      return text.includes(this.search);
+    }.bind(this));
+    this.searchCategories = this.searchCategories.sort(function(a, b){
+      if(a.firstname < b.firstname) return -1;
+      if(a.firstname > b.firstname) return 1;
+      return 0;
+    });
+    /*
     var categoryData = this.ds.dataRecord.get('categories');
     this.categories = [];
     for (var category in categoryData) {
@@ -46,7 +81,7 @@ export class HomePage {
     }
     this.categories = this.categories.filter(function(text) {
       return text.includes(this.search);
-    }.bind(this));
+    }.bind(this));*/
 
 //    this.tutors = tutorsData.filter(function(text) {
 //      return text.includes(this.search);
