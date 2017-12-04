5fe437030f959eba4bfc3e759fe4917434db3d21
Robin Luo <RPHLuo@github.com>
2017-12-04 16:19:15 -0500

    finished onboarding ui

--- React-Native/app/components/onboard.js
+++ React-Native/app/components/onboard.js
@@ -8,6 +8,7 @@ import {
   Platform,
   StyleSheet,
   FlatList,
+  ScrollView,
   Text,
   Image,
   View
@@ -28,21 +29,53 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   container: {
-    flex: 0.5
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
   },
-  imageContainer: {
-    flex: 1
+  selectedImage: {
+    flex: 1,
+    resizeMode: 'contain'
   },
   image: {
+    tintColor: '#208',
+    flex: 1,
     resizeMode: 'contain'
   },
   descContainer: {
+    flex: 1,
     flexDirection: 'column'
   },
   title: {
+    flex: 1,
+    fontSize: 30,
+    minHeight: 25
   },
   textInput: {
-    borderWidth: 1
+    flex: 9,
+    borderWidth: 1,
+    margin: 15,
+    marginTop: 0,
+    textAlignVertical: 'top'
+  },
+  tosContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start'
+  },
+  tosTitle: {
+    flex: 1,
+    padding: 15,
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  tos: {
+    flex: 8
+  },
+  accept: {
+    flex: 1,
+    marginBottom: 40
   }
 });
 
@@ -53,7 +86,8 @@ export default class Onboard extends Component<{}> {
     this.state = {
       modalVisible: true,
       tutor: false,
-      subjects:[]
+      subjects:[],
+      showAccept: false
     }
   }
 
@@ -74,8 +108,17 @@ export default class Onboard extends Component<{}> {
     }
   }
 
+  showAccept() {
+    this.setState({
+      modalVisible: this.state.modalVisible,
+      tutor: this.state.tutor,
+      subjects:this.state.subjects,
+      showAccept: true
+    })
+  }
+
   renderRole(role, selected, onSelect, index) {
-    const style = selected ? { fontWeight: 'bold'} : {};
+    const style = selected ? { fontWeight: 'bold', color: '#208', fontSize: 20, marginTop: 15} : {fontSize: 20, marginTop: 15};
     return (
       <TouchableOpacity
         onPress={ onSelect }
@@ -83,16 +126,22 @@ export default class Onboard extends Component<{}> {
         style= { styles.container }
       >
         <View
-          style={ styles.imageContainer }
+          style= {{ flex: 1,flexDirection:'row'}}
         >
           <Image
-            style={ styles.image }
+            style={ selected? styles.selectedImage : styles.image }
             source={ role === "Tutor"? require('../assets/tutor.png') : require('../assets/student.png') }
           />
         </View>
-          <Text>
+        <View
+          style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}
+        >
+          <Text
+            style = { style }
+          >
             { role }
           </Text>
+        </View>
       </TouchableOpacity>
     );
   }
@@ -125,41 +174,73 @@ export default class Onboard extends Component<{}> {
                   options = { ['Student', 'Tutor' ] }
                   onSelection={ this.selectRole.bind(this) }
                   renderOption={ this.renderRole }
-                  renderContainer={RadioButtons.renderHorizontalContainer}
+                  renderContainer={RadioButtons.getViewContainerRenderer({flex:1, flexDirection: 'row', justifyContent: 'center'})}
                   optionContainerStyle= { styles.radio }
-                />
-                <FlatList
-                  data={ this.state.subjects }
-                  renderItem={ ({ item }) => <Text>{ item.subject }</Text> }
-                />
-                <FlatList
-                  data={ this.state.results }
-                  renderItem={ ({ item }) => <Text>{ item.subject }</Text> }
                 />
               </View>
               <View
                 style={ styles.descContainer}
               >
-                <Text
-                  style={ styles.title }
+                <View
+                  style={{ flex:1, flexDirection:'row', justifyContent: 'center', margin: 15, marginBottom: 0 }}
                 >
-                  Tell us about yourself
-                </Text>
+                  <Text
+                    style={ styles.title }
+                  >
+                    Tell us about yourself
+                  </Text>
+                </View>
                 <TextInput
                   multiline
                   style={ styles.textInput }
+                  underlineColorAndroid="transparent"
                   onChangeText={ (text) => this.state.description = text }
                   value={ this.state.description }
                 />
               </View>
-              <View>
-                <Text>
-                  Terms and Conditions
-                </Text>
-                <Button
-                  onPress={ this.register.bind(this) }
-                  title="Accept"
-                />
+              <View
+                style= { styles.tosContainer}
+              >
+                <View
+                  style={ styles.tosTitle }
+                >
+                  <Text
+                    style={ styles.title }
+                  >
+                    Terms and Conditions
+                  </Text>
+                </View>
+                <View
+                  style={ styles.tos }
+                >
+                  <ScrollView
+                    onScroll={ this.showAccept.bind(this) }
+                  >
+                    <Text
+                      style={{padding: 30, fontSize: 16}}
+                    >
+                      “As Is” Services. Blindside Networks Inc. (“Blindside Networks”) grants You a non-exclusive, non-transferable, limited, and revocable license to use select Blindside Networks services (“Services”) for personal, non-commercial purposes and in accordance with these Terms of Use. You acknowledge and agree that all Services provided to You by Blindside Networks pursuant to these Terms of Use are on an “as is” basis, without representations, warranties, and condition of any kind and may be modified or discontinued by Blindside Networks at any time. To the extent that the Services relate to the BigBlueButton open source software project, your license to that software is subject to the licenses then in effect for it in addition to these Terms of Use.
+                      {'\n'}
+                      Use & Content. You acknowledge and agree that the Services may not be used in violation of any legislation or license applicable to You or that is any way fraudulent or unlawful including without limitation, to send, knowingly receive, download, upload, use or re-use any audio, video, text, or images (“Content”) that is abusive, defamatory, in breach of confidence, obscene or menacing, in breach of copyright or privacy or any other rights; or to damage Blindside Networks’ property, interfere with or disrupt Blindside Networks’ system or other users. You agree to allow Blindside Networks and its applicable contractors to freely host, reproduce, transmit, modify, display and otherwise use your Content (in whole or in part) as reasonably necessary to provide the Services to you. You acknowledge and agree that Blindside Networks does not control or monitor the Content nor guarantee the accuracy, integrity, security or quality of such Content or the Services.
+                      {'\n'}
+                      Termination & Removal of Content. Blindside Networks reserves the right to terminate Your license to use the Services at any time in Blindside Networks’ sole and unfettered discretion. Blindside Networks also reserves the right to remove Content which it deems, in its sole discretion, will or may subject it to liability or is dangerous, offensive, pornographic, or in violation of law or regulations currently in effect. Such termination or removal may be immediate and without notice.
+                      {'\n'}
+                      Indemnification. You Customer agree to indemnify, defend, and hold harmless Blindside Networks and its directors, officers, employees, members, volunteers, affiliates, subcontractors, independent contractors, distributors, and agents from and against all losses, damages, actions or causes of action, suits, claims, demands, penalties and interest arising in connection with or out of your use of the Services.
+                      {'\n'}
+                      These Terms of Use are the final, complete and exclusive agreement of the parties with respect to the subject matter hereof; they supersede and merge all prior discussions between the parties with respect to such subject matter.
+                      {'\n'}
+                      January 19, 2017
+                    </Text>
+                  </ScrollView>
+                </View>
+                <View
+                  style = { this.state.showAccept? styles.accept : {display:'none'} }
+                >
+                  <Button
+                    onPress={ this.register.bind(this) }
+                    title="Accept"
+                  />
+                </View>
               </View>
             </Swiper>
         </Modal>
