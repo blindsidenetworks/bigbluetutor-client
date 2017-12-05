10d824782627d664ff061e0d69b188ec4814d6bc
Robin Luo <RPHLuo@github.com>
2017-12-05 14:40:35 -0500

    styled messages and partially inbox

--- React-Native/app/components/inbox.js
+++ React-Native/app/components/inbox.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 
 import NavigationBar from './navigationBar';
-import { Card } from 'react-native-elements';
+import { Card, ListItem } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 
 export default class InboxPage extends Component<{}> {
@@ -53,18 +53,16 @@ export default class InboxPage extends Component<{}> {
         <FlatList
           data={ this.state.messagesKeys }
           extraData={ this.state.messagesKeys }
-          renderItem={({item}) =>
-            <Card
+          renderItem={({ item }) =>
+            <ListItem
+              roundAvatar
+              avatar={{ uri: this.state.messages[item].pic }}
+              key={ item }
               title={ item }
-              image={{ uri: this.state.messages[item].pic }}
-            >
-            <Button
-              title="click"
-              onPress={() => { Actions.messages({props: this.props, username: item})}}
+              onPress={ () => { Actions.messages({ props: this.props, username: item })}}
             />
-            </Card>
           }
-          keyExtractor = {(item, index) => item}
+          keyExtractor = { (item, index) => item }
         />
       </View>
     );
